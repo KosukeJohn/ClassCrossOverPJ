e952184f5cd370881c8a1f7e702f79643d6c17b4
Bisu1noshin <bisu8979@icloud.com>
2024-10-25 23:24:12 +0900

    jump1_3人称視点

---
+++ CI/SamplePJ Sakamoto/Assets/Camera_Com/Camera_1.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_1 : MonoBehaviour
+{
+    //ゲームオブジェクトの定義
+    public GameObject Camera1;
+    public GameObject Player;
+
+    private Transform   cameraTrans;//CameraのRotaiton
+    private Transform   playerTrans;//PlayerのRotaiton
+    private float       AngleSpeed;//回転速度
+    private Vector3     Angle_axis;//回転の軸
+
+    void Start()
+    {
+        //初期化
+        cameraTrans = Camera1.transform;
+        playerTrans = Player.transform;
+
+        //Playerの回転速度を取得
+        AngleSpeed = Player.GetComponent<Player>().ro_speed;
+
+        //CameraをPlayerの位置に初期化
+        Camera1.transform.rotation = Player.GetComponent<Transform>().rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Cameraの座標の更新
+        {
+            Vector3 PlayerPos = Player.transform.localPosition;
+            Camera1.transform.localPosition =
+                new Vector3(PlayerPos.x, transform.localPosition.y, PlayerPos.z);//Playerの後ろの位置に更新
+        }
+
+        //Cameraの角度
+        {
+            //回転軸の取得
+            Angle_axis = playerTrans.up;
+
+            //yの遷移
+            float angle = 0;
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                angle = -AngleSpeed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                angle = AngleSpeed * Time.deltaTime;
+            }
+
+            cameraTrans.localRotation = //Quaternion.AngleAxis(angle, Angle_axis)で指定した軸で回転させる
+                Quaternion.AngleAxis(angle, Angle_axis) * cameraTrans.localRotation;
+        }      
+    }
+}
