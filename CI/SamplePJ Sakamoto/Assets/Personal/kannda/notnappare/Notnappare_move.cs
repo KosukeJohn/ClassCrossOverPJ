17954ad91b7c9c4c2e154293f22f393555c32ade
Bisu1noshin <bisu8979@icloud.com>
2024-12-06 15:17:07 +0900

    komitto

---
+++ CI/SamplePJ Sakamoto/Assets/Personal/kannda/notnappare/Notnappare_move.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notnappare_move : MonoBehaviour
+{
+    private GameObject enemy;
+    private GameObject leader;
+
+    private void Start()
+    {
+        leader= GameObject.Find("");
+        enemy = this.gameObject;
+    }
+
+
+}
