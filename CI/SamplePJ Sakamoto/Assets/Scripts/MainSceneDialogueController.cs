10df6c9345bdde7028f3e0214364704bbb29f72c
KosukeJohn <kosukemusi@gmail.com>
2024-12-08 17:42:04 +0900

    色々　キーアイテムを取ってボスが出てきて、エンディングまでの流れ追加

--- CI/SamplePJ Sakamoto/Assets/Scripts/MainSceneDialogueController.cs
+++ CI/SamplePJ Sakamoto/Assets/Scripts/MainSceneDialogueController.cs
@@ -1,23 +1,24 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.InputSystem;
 
 public class MainSceneDialogueController : MonoBehaviour
 {
-    GameObject dialogueObject; // ダイアログ用のUIオブジェクト
-    Text dialogueText; // ダイアログのTextコンポーネントをキャッシュ
+    [Header("ダイアログ設定")]
+    public GameObject dialogueObject; // ダイアログ用のUIオブジェクト
+    private Text dialogueText; // ダイアログのTextコンポーネントをキャッシュ
 
-    // 表示する文章のリスト
+    [Header("表示する文章")]
     private string[] dialogues = {
-        "ようこそ、冒険者！",
-        "この地には数多くの試練が待っています。",
-        "準備はできていますか？",
-        "さあ、冒険を始めましょう！"
+        "主人公「僕人形になってる！」",
+        "主人公「蝶ネクタイがない・・・」",
+        "マリオネット「僕らを大切にしなかったから君は人形になったんだ！」",
+        "マリオネット「人間に戻りたかったら、取りにおいで！」",
+        "主人公「とりあえず進もう」"
     };
 
     private int currentDialogueIndex = 0; // 現在の文章のインデックス
+    private Coroutine dialogueCoroutine; // ダイアログ進行用のコルーチン
 
     // Start is called before the first frame update
     void Start()
@@ -25,38 +26,67 @@ public class MainSceneDialogueController : MonoBehaviour
         // シーン内で名前が "Dialogue" のオブジェクトを探す
         dialogueObject = GameObject.Find("Dialogue");
 
-        // ダイアログのTextコンポーネントを取得して初期設定
-        dialogueText = dialogueObject.GetComponent<Text>();
-      
+        // ダイアログのTextコンポーネントを取得
+        if (dialogueObject != null)
+        {
+            dialogueText = dialogueObject.GetComponent<Text>();
+        }
+        else
+        {
+            Debug.LogError("Dialogueオブジェクトが見つかりません。");
+            return;
+        }
+
+        // ダイアログの初期文章を設定して表示
+        if (dialogueText != null)
+        {
+            dialogueText.text = dialogues[currentDialogueIndex];
+        }
+
+        // 1秒ごとにテキストを進めるコルーチンを開始
+        dialogueCoroutine = StartCoroutine(AdvanceDialogueAutomatically());
     }
 
-    // Update is called once per frame
-    void Update()
+    // ダイアログを自動的に進めるコルーチン
+    IEnumerator AdvanceDialogueAutomatically()
     {
-       
+        while (currentDialogueIndex < dialogues.Length - 1)
+        {
+            yield return new WaitForSeconds(2f); // 1秒待機
+            currentDialogueIndex++; // インデックスを進める
+            dialogueText.text = dialogues[currentDialogueIndex]; // 次の文章を表示
+        }
+
+        // 最後のダイアログが終了したら非表示にする
+        yield return new WaitForSeconds(1f);
+        dialogueText.enabled = false;
+        Debug.Log("すべてのダイアログが終了しました。ダイアログを非表示にしました。");
     }
 
-    public void onEnter()
+    // 手動でダイアログを進める場合（ボタンイベント等で使用）
+    public void OnEnter()
     {
-        Debug.Log("Aボタンが押されました！");
+        if (dialogueCoroutine != null)
+        {
+            StopCoroutine(dialogueCoroutine); // 自動進行を停止
+            dialogueCoroutine = null;
+        }
 
-
-        AdvanceDialogue();
+        AdvanceDialogueManually();
     }
 
-    // 次のダイアログに進む
-    void AdvanceDialogue()
+    // 手動で次のダイアログに進む
+    void AdvanceDialogueManually()
     {
-        if (dialogueText != null && currentDialogueIndex < dialogues.Length - 1)
+        if (currentDialogueIndex < dialogues.Length - 1)
         {
             currentDialogueIndex++; // インデックスを進める
             dialogueText.text = dialogues[currentDialogueIndex]; // 次の文章を表示
         }
         else
         {
-            // ダイアログが終了したら非表示にする
-            dialogueText.enabled = false;
-            Debug.Log("すべてのダイアログが終了しました。ダイアログを非表示にしました。");
+            dialogueText.enabled = false; // ダイアログを非表示にする
+            Debug.Log("すべてのダイアログが終了しました。");
         }
     }
 }
