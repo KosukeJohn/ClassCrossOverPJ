b032bea8176b5695b0cea94354325ac3ec727d8a
JEC karato <24ci0112@jec.ac.jp>
2025-01-30 13:25:24 +0900

    シーン回収

---
+++ CI/SamplePJ Sakamoto/Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EndGame();
+    }
+
+    void EndGame()
+    {
+        if(Input.GetKeyUp(KeyCode.Escape))
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;//ゲーム終了
+#else
+    Application.Quit();//ゲーム終了
+#endif
+        }
+    }
+
+}
