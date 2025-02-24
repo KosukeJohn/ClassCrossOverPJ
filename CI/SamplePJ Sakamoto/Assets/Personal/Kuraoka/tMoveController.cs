a1ccac158ab9242a0d2bf83c2256dd32082c08ef
Kazu-ci <kazushik0422@gmail.com>
2025-02-25 02:21:46 +0900

    UI差し替え改訂版 モーションつけるのガチきつかったって

---
+++ CI/SamplePJ Sakamoto/Assets/Personal/Kuraoka/tMoveController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class tMoveController: MonoBehaviour
+{
+   
+    RectTransform rectTransform;
+    [SerializeField] float posy;
+    [SerializeField] float posx;
+    [SerializeField] float posz;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform=GetComponent<RectTransform>();
+        posx=rectTransform.position.x;
+        posy = rectTransform.position.y;
+        posz=rectTransform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        posy -= 350*Time.deltaTime;
+        rectTransform.position = new Vector3(posx, posy, posz);
+        if(posy<800)
+        {
+            posy = 800;
+            rectTransform.position = new Vector3(posx, posy, posz); 
+        }
+       
+    }
+}
